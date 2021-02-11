0872638fa635949ed3111ad6b49db5727ad367ca
Mahaalakshmik <haarikha06@gmail.com>
2021-02-11 14:16:07 +0530

    checking

---
+++ src/test/java/com/tcs/Amazon/test1.java
@@ -0,0 +1,14 @@
+package com.tcs.Amazon;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mahaalakshmi K\\eclipse-workspace\\Amazon\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
